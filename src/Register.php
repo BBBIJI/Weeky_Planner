f28686a317b14007177043938bae66e6319f08ba
BBBIJI <liekenny273@gmail.com>
2023-05-30 07:58:48 +0800

    Big change
    -Added login page
    -Added register page
    -Added databse and connection

---
+++ src/Register.php
@@ -0,0 +1,18 @@
+<?php
+$Username = $_POST['Username'];
+$Password = $_POST['Password'];
+
+//Make database connection
+$conn = new mysqli('localhost','root','','user');
+if($conn->connect_error){
+    die('Connection Failed : '.$conn->connect_error);
+}else{
+    $stmt = $conn->prepare("insert into login(Username, Password) values(?,?)");
+    $stmt->bind_param("ss",$Username,$Password);
+    $stmt->execute();
+    echo "registetration success";
+    $stmt->close();
+    $conn->close();
+}
+?>
+
