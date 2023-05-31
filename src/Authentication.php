f808d4de6cffb55cd2525ede1a59c5f83456bf13
BBBIJI <liekenny273@gmail.com>
2023-05-31 21:51:05 +0800

    bigbig

--- src/Authentication.php
+++ src/Authentication.php
@@ -16,7 +16,7 @@ $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
 $count = mysqli_num_rows($result);
 
 if($count == 1){
-    header("Location: wp.html");
+    header("Location: wp.php");
     die();
 }else{
     echo "<h1><center>Login Failed</center></h1>";
--- src/connection.php
+++ src/connection.php
@@ -7,5 +7,5 @@
     $con = mysqli_connect($host, $User,$Pass, $db_name);  
     if(mysqli_connect_errno()) {  
         die("Failed to connect with MySQL: ". mysqli_connect_error());  
-    }  
+    }
 ?>
